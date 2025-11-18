a7e898909cdda533479a960d81fd90b130fe544b
Wellington Bragança <braganca.wellington@icloud.com>
2025-11-17 20:07:58 -0400

    Atualizando criação projeto

---
+++ src/main/java/com/projetosprbt/workshop/WorkshopprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.projetosprbt.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopprojetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetosprbt/workshop/WorkshopprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosprbt.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
