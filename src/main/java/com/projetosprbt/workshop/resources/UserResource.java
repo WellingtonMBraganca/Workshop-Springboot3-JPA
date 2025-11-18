77dd5e9b281eeaf076f77e6463e3316ffca87b9b
Wellington Bragança <braganca.wellington@icloud.com>
2025-11-17 20:45:30 -0400

    User entity and resource

---
+++ src/main/java/com/projetosprbt/workshop/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.projetosprbt.workshop.resources;
+
+import com.projetosprbt.workshop.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Anotação indica que este é um recurso web, implementado por controlador rest.
+@RestController
+//da nome ao recurso.
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    //tipo especifico que retorna resposta para requisições web.
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u =new User(1L, "Godofredo", "godofs@arromba.com", "9988776655", "123456");
+        return ResponseEntity.ok().body(u);
+    };
+}
