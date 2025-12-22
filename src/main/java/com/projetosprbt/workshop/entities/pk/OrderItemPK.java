d1d8456d4cd0468975eacb5ebcf30b5ef99ed0c1
Wellington Bragança <braganca.wellington@icloud.com>
2025-12-22 15:50:55 -0400

    OrdemItem pt1

---
+++ src/main/java/com/projetosprbt/workshop/entities/pk/OrderItemPK.java
@@ -0,0 +1,50 @@
+package com.projetosprbt.workshop.entities.pk;
+//classe auxiliar para associar chave estrangeira de itens do pedido
+//uma vez que em orienttação objeto, não temos uma chave que associe ambas tabelas.
+
+import com.projetosprbt.workshop.entities.Order;
+import com.projetosprbt.workshop.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
