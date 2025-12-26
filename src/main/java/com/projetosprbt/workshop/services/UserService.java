72f68e1b406293e2477266b991a51162375d1dee
Wellington Bragança <braganca.wellington@icloud.com>
2025-12-26 19:11:54 -0400

    Exception handler delete

--- src/main/java/com/projetosprbt/workshop/services/UserService.java
+++ src/main/java/com/projetosprbt/workshop/services/UserService.java
@@ -2,8 +2,11 @@ package com.projetosprbt.workshop.services;
 
 import com.projetosprbt.workshop.entities.User;
 import com.projetosprbt.workshop.repositories.UserRepository;
+import com.projetosprbt.workshop.services.exceptions.DatabaseException;
 import com.projetosprbt.workshop.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -38,7 +41,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e ){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/com/projetosprbt/workshop/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.projetosprbt.workshop.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException (String msg){
+        super(msg);
+    }
+}
